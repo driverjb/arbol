1432ff54c4fb43358e18bd4f63a3af9eaad87084
Joel Driver <driverjb09@gmail.com>
2020-07-31 17:00:59 -0400

    tree config schema

---
+++ tree/tree.schema.js
@@ -0,0 +1,10 @@
+const Joi = require('@hapi/joi');
+
+module.exports = Joi.object().keys({
+  host: Joi.string().valid('0.0.0.0', 'localhost').default('0.0.0.0').optional(),
+  port: Joi.number().default(3000).optional(),
+  production: Joi.boolean()
+    .default(process.env.NODE_ENV === 'production')
+    .optional(),
+  trustProxy: Joi.alternatives().try(Joi.boolean(), Joi.number()).default(false).optional(),
+});
