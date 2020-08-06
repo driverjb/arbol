82cd4ad1eb04029f2b7584f5ea39d6730d74928d
Joel Driver <driverjb09@gmail.com>
2020-08-06 16:33:21 -0400

    changed dependency to latest

--- tree/tree.schema.js
+++ tree/tree.schema.js
@@ -1,4 +1,4 @@
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 
 module.exports = Joi.object().keys({
   host: Joi.string().valid('0.0.0.0', 'localhost').default('0.0.0.0').optional(),
