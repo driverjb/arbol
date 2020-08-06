c074ec333433c984570c5c45c41c0a1d0072e7d7
Joel Driver <driverjb09@gmail.com>
2020-08-06 16:34:28 -0400

    export all functionality

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 module.exports.Tree = require('./tree/tree.class');
+module.exports.Branch = require('./branch/branch.class');
+module.exports.Leaf = require('./leaf/leaf.class');
+module.exports.ArbolError = require('./util/arbolError.class');
