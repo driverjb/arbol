6d6d41173e15c13a2faa46fe67085d2ecfae95ba
Joel Driver <driverjb09@gmail.com>
2020-08-10 16:23:42 -0400

    export the security class for use

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ module.exports.Tree = require('./tree/tree.class');
 module.exports.Branch = require('./branch/branch.class');
 module.exports.Leaf = require('./leaf/leaf.class');
 module.exports.ArbolError = require('./util/arbolError.class');
+module.exports.Security = require('./util/security.class');
