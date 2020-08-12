b1bacb18d674c06165222f2f17974866ebabfc6f
Joel Driver <driverjb09@gmail.com>
2020-08-12 08:30:28 -0400

    added 'all' method handler

--- leaf/leaf.schema.js
+++ leaf/leaf.schema.js
@@ -1,7 +1,10 @@
 const Joi = require('joi');
 
 const LeafSchema = Joi.object().keys({
-  method: Joi.string().valid('get', 'put', 'patch', 'post', 'delete').default('get').optional(),
+  method: Joi.string()
+    .valid('get', 'put', 'patch', 'post', 'delete', 'all')
+    .default('get')
+    .optional(),
   responder: Joi.func().required(),
   path: Joi.string().required(),
   twigs: Joi.array().items(Joi.func()).optional()
