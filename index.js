36b6eb451ae71e61cdb8849387503fb552326739
driverjb <driverjb09@gmail.com>
2020-10-22 09:58:57 -0400

    typo

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports.ArbolError = require('./util/arbolError.class');
 module.exports.Branch = require('./util/branch.class');
 module.exports.Leaf = require('./util/leaf.class');
 module.exports.Security = require('./util/security.class');
-module.exports.arbol = require('./util/Arbol.util');
+module.exports.arbol = require('./util/arbol.util');
 module.exports.validators = require('./util/validators.util');
