b54b323549df7b3fe8f2e9a4ae9b062a2fcdf1f8
driverjb <driverjb09@gmail.com>
2020-10-27 12:01:00 -0400

    added token decoding and comment blocks

--- util/security.class.js
+++ util/security.class.js
@@ -12,14 +12,30 @@ class Security {
     this.privateKey = privateKey;
     this.algorithm = algorithm;
   }
+  /**
+   * Sign a new JWT by merging the payload with the default JWT payload
+   * @param {Object} payload
+   * @param {string} [ttl="24h"] Time format: 1d, 24h, 1440m, 86400s. Default: 24h
+   */
   sign(payload, ttl = '24h') {
     return jwt.sign(payload, this.privateKey, {
       algorithm: this.algorithm,
       expiresIn: ttl
     });
   }
+  /**
+   * Validate a token and return its payload
+   * @param {string} token
+   */
   verify(token) {
     return jwt.verify(token, this.privateKey, { algorithms: [this.algorithm] });
+  }
+  /**
+   * Return a token's payload. BEWARE! No validation is performed.
+   * @param {string} token
+   */
+  decode(token) {
+    return jwt.decode(token);
   }
 }
 
