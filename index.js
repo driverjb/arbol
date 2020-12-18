0e3e4834f6a719749141cea42ea9f706691f3d7e
driverjb <driverjb09@gmail.com>
2020-12-18 15:27:55 -0500

    removed unneeded service

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
   ArbolError: require('./lib/ArbolError.class'),
-  Service: require('./lib/Service.class'),
   Tree: require('./lib/Tree.class'),
   Leaf: require('./lib/Leaf.class'),
   Branch: require('./lib/Branch.class'),
