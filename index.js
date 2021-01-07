ae035ac6f0190baa46b994bc2a220c3da19b0822
driverjb <driverjb09@gmail.com>
2021-01-06 22:53:41 -0500

    added new objects that can be returned from services to control how a response is made rather that locking the response to the api configuration

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ module.exports.ArbolError = require('./lib/ArbolError.class');
 module.exports.Branch = require('./lib/Branch.class');
 module.exports.Check = require('./lib/Check.module');
 module.exports.Tree = require('./lib/Tree.class');
+module.exports.CSVFile = require('./lib/CSVFile.class');
+module.exports.CSVHeader = require('./lib/CSVHeader.class');
+module.exports.Cookie = require('./lib/Cookie.class');
